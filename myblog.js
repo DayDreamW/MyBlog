21f1781744bc0129d97a60f15bc26016a84eb5ff
王梅美 <wangmeimei@rotongroup.com>
2018-03-15 17:00:07 +0800

    Initial commit.

---
+++ myblog.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.set('port',process.env.PORT || 3000);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req, res){
+	res.type('text/plain');
+	res.send('MyBlog Look');
+});
+
+app.get('/about', function(req,res){
+	res.type('text/plain');
+	res.send('MyBlog about');
+})
+//定制404页面
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404)
+});
+	
+//定制500页面
+app.use(function(err,req,res,next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+	
+})
+
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + 
+		app.get('port') + '; press Ctrl-C to terminate' )
+})
